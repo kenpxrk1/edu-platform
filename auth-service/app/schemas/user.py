50f3e91345ae594cd3b4f4fcd12a40730dce8a4a
Dmitry Grishin <kenpxrkasf@gmail.com>
2024-06-17 17:09:29 +0300

    created validation for user

---
+++ auth-service/app/schemas/user.py
@@ -0,0 +1,98 @@
+import re
+from fastapi import HTTPException
+from pydantic import BaseModel, EmailStr, field_validator
+import datetime as dt
+from app.models.user import Role
+
+
+NAMING_PATTERN = re.compile(r"^[а-яА-Яa-zA-Z]+$")
+
+
+class CreateUser(BaseModel):
+    email: EmailStr
+    name: str 
+    surname: str
+    password: str
+    birth_date: dt.date
+
+    @field_validator('name', 'surname')
+    def name_validator(value: str):
+        """ Проверяет, что выбранные поля содержут
+         только символы русского и латинского алфавита,
+         а также не являются пустыми. 
+        """
+
+        if value.isspace():
+            raise HTTPException(
+                status_code=422,
+                detail=f"Поле {value} не может быть пустым."
+            )
+
+        if not NAMING_PATTERN.match(value):
+            raise HTTPException(
+                status_code=422,
+                detail=f"Поле {value} содержит недопустимые символы."
+            )
+        return value
+    
+    @field_validator('password')
+    def password_validator(password: str):
+        """ Проверяет, что пароль:
+        состоит из 8+ символов, не состоит из одного символа,
+        не состоит из пробелов.
+        """
+        if len(password) < 8:
+            raise HTTPException(
+                status_code=422,
+                detail=f"Пароль слишком короткий"
+            )
+        
+        if password.count(' ') > 0 or password.isspace():
+            raise HTTPException(
+                status_code=422,
+                detail=f"Пароль cодержит недопустимые символы"
+            )
+        
+        symbol = password[0]
+        if password.count(symbol) == len(password):
+            raise HTTPException(
+                status_code=422,
+                detail=f"Введите пароль надежнее"
+            )
+        
+        return password
+        
+
+    @field_validator('birth_date')
+    def birth_date_validator(birth_date: dt.date):
+        current_date =  dt.date.today()
+        
+        if birth_date > current_date:
+            raise HTTPException(
+                status_code=400,
+                detail="Дата рождения не может быть позже чем текущая дата."
+            )
+        
+        if abs(int(current_date.year) - int(birth_date.year)) >= 115: 
+            raise HTTPException(
+                status_code=400,
+                detail="Недопустимая дата рождения"
+            )
+
+
+
+class LoginUser(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class GetUser(BaseModel):
+    id: int
+    name: str
+    surname: str
+    email: str 
+    role: Role
+    birth_date: str
+
+class UpdateUser(CreateUser):
+    pass 
